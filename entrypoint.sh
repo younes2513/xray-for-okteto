21aebc484e45ac7bedc1a223804c1f5c8aab9465
younes2513 <124598454+younes2513@users.noreply.github.com>
2023-04-17 20:09:01 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
+UUID='98c7ca40-97f0-4016-a826-a2213ba9180a'
+VMESS_WSPATH='/speedvmess'
+VLESS_WSPATH='/speedvless'
+TROJAN_WSPATH='/speedtrojan'
 SS_WSPATH='/shadowsocks'
 NEZHA_SERVER=''
 NEZHA_PORT=''
